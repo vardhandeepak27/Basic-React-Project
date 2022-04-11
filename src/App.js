2d779d4fa857f05fe8e91a76396972cb0da1c153
Vardhan <vardhandeepak1234@gmail.com>
2022-04-11 09:55:25 +0530

    Updated till router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
-/*import Textform from './components/Textform';*/
+import Textform from './components/Textform';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 
 function App() {
+
+  const[mode,setMode]=useState('light');
+  const[rmode,rsetMode]=useState('light');
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor="#042743"
+      }
+    
+    else{
+      setMode('light');
+      document.body.style.backgroundColor="white"
+      }
+      
+  }
+
+  const rtoggleMode=()=>{
+    if(rmode==='light'){
+      rsetMode('#F88379');
+      document.body.style.backgroundColor="#F88379"
+    }
+    else{
+      rsetMode('light');
+      document.body.style.backgroundColor="white"
+    }
+  }
+
+
   return (
     <>
-    <Navbar title="TextUtils" about="About TextUtils"/>
-    <div className='container'>
-      {/*<Textform heading="Enter your Text here"/>*/}
-      <About/>
-    </div>
-  
+        <BrowserRouter>
+        <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode} rmode={rmode} rtoggleMode={rtoggleMode}/>
+      <Routes>
+        <Route exact path="/" element={<Textform heading="Enter your Text here" mode={mode}/>}/>
+          <Route exact path="/about" element={<About />}/>
+          
+      
+        
+      </Routes>
+      
+    </BrowserRouter>,
     
     
-    </>
+    
+      </>
   );
 }
 
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -17,27 +17,31 @@ export default function Textform(props) {
         setText(event.target.value)
     }
 
+    const handleCopy=()=>{
+      var text = document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
+
     const[text,setText]=useState("")
   return (
       <>
-      <div className="container">
+      <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
       <h2>{props.heading}</h2>
 <div className="mb-3">
-  <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
+  <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
   <button className="btn btn-primary m-3 " onClick={handleUpClick}>Convert to UpperCase</button>
   <button className="btn btn-primary m-3 " onClick={handleLowClick}>Convert to LowerCase</button>
   <button className="btn btn-primary m-3 " onClick={handleRemoveText}>Clear Text</button>
-</div>
-</div>
-
-<div className="container">
-  
-  <h2>Your Text Summary</h2>
+  <button className="btn btn-primary m-3 " onClick={handleCopy}>Copy Text</button>
+    <h2>Your Text Summary</h2>
   <p>No of characters are {text.length} and No of words are {text.split(" ").length}</p>
   <h2>Preview</h2>
   <p>{text}</p>
 
 </div>
+</div>
+
 </>
 
   )
